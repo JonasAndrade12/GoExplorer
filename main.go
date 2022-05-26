30638cdbc631cc937e5b4dcb0d41e0cbbc425a07
JonasAndrade12 <jonas_andrade@live.com.pt>
2022-05-27 00:02:29 +0100

    feat: explore functions

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "3 ♣"
-	card2 := "2 ♥"
+	card := "2 ♥"
+	card2 := newCard()
 	fmt.Println(card)
 	fmt.Println(card2)
+}
+
+func newCard() string {
+	return "5 ♢"
 }
