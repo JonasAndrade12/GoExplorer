9310b13335d178785ccdc32ebd869edae6a30b65
Jonas Andrade <jonas.andrade@farfetch.com>
2022-07-14 14:53:10 +0100

    chore: clean unnecessary pointer variable

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 func main() {
 	cards := newDeck()
-	pointerToDeck := &cards
-	pointerToDeck.addCard(card{Value: "R", Suit: "♡"})
+	cards.addCard(card{Value: "R", Suit: "♡"})
 	cards.saveToFile("my_cards")
 	readFromFile("my_cards")
 	cards.shuffle()
